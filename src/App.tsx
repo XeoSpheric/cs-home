eb6bb757cf8c64bd89cfb61c33bea376ebca5a39
XeoSpheric <calvin@xeosmoot.com>
2022-02-26 10:12:31 -0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,57 @@
 import { useState } from "react";
-import logo from "./logo.svg";
+import { Routes, Route, Link } from "react-router-dom";
+import { AppShell, Navbar, Header, Autocomplete, Grid, Input, InputWrapper } from "@mantine/core";
 import "./App.scss";
+import {
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+} from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates. Hot dnag.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <AppShell
+      padding="md"
+      navbar={
+        <Navbar width={{ base: 300 }} height={500} padding="xs">
+          
+        </Navbar>
+      }
+      header={
+        <Header height={60} padding="xs">
+          <Grid gutter={'sm'} columns={24} align={'center'}>
+            <Grid.Col span={1}>
+              <img src="/logo.png" className="w-8"></img>
+            </Grid.Col>
+            <Grid.Col span={4}>
+              <span>xeosmoot.com</span>
+            </Grid.Col>
+            <Grid.Col span={8}>
+              <InputWrapper
+                id="global-search"
+                size="md">
+                <Input id="global-search" placeholder="Search" />
+              </InputWrapper>
+            </Grid.Col>
+          </Grid>
+        </Header>
+      }
+      styles={(theme) => ({
+        main: {
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[8]
+              : theme.colors.gray[0],
+        },
+      })}
+    >
+      <Autocomplete
+        label="Your favorite framework/library"
+        placeholder="Pick one"
+        data={["React", "Angular", "Svelte", "Vue"]}
+      />
+    </AppShell>
   );
 }
 
