af8a84226c6dc6d43d04a4de856721d6c5c0db64
Calvin Smoot <calvin@xeosmoot.com>
2022-03-10 16:43:36 -0700

    Stupid little fix

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,10 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Stupid thing to get no errors for deployement
+const api: string = import.meta.env.VITE_FIREBASE_API as string;
+const auth: string = import.meta.env.VITE_FIREBASE_AUTH as string;
+const id: string = import.meta.env.VITE_FIREBASE_PROJECT_ID as string;
+
 const firebaseApp = initializeApp({
-  apiKey: import.meta.env.VITE_FIREBASE_API,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  apiKey: api,
+  authDomain: auth,
+  projectId: id,
 });
 
 const db = getFirestore();
