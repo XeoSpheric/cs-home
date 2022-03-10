2be80f6a12629ed183c439cba5ec9c905bf88b18
Calvin Smoot <calvin@xeosmoot.com>
2022-03-10 16:31:31 -0700

    Running a build

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -1,14 +1,13 @@
-
-import { useState } from 'react';
-import { PasswordInput, Progress, Text, Popover, Box } from '@mantine/core';
-import { Check, Crosshair } from 'tabler-icons-react';
-import Requirement from '../models/passwordRequirement';
+import { useState } from "react";
+import { PasswordInput, Progress, Text, Popover, Box } from "@mantine/core";
+import { Check, Crosshair } from "tabler-icons-react";
+import Requirement from "../models/passwordRequirement";
 
 const requirements: Requirement[] = [
-  { re: /[0-9]/, label: 'Includes number' },
-  { re: /[a-z]/, label: 'Includes lowercase letter' },
-  { re: /[A-Z]/, label: 'Includes uppercase letter' },
-  { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: 'Includes special symbol' },
+  { re: /[0-9]/, label: "Includes number" },
+  { re: /[a-z]/, label: "Includes lowercase letter" },
+  { re: /[A-Z]/, label: "Includes uppercase letter" },
+  { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: "Includes special symbol" },
 ];
 
 const getStrength = (password: string) => {
@@ -21,19 +20,25 @@ const getStrength = (password: string) => {
   });
 
   return Math.max(100 - (100 / (requirements.length + 1)) * multiplier, 10);
-}
+};
 
-const PasswordRequirement = ({ meets, label }: { meets: boolean; label: string }) => {
+const PasswordRequirement = ({
+  meets,
+  label,
+}: {
+  meets: boolean;
+  label: string;
+}) => {
   return (
     <Text
-      color={meets ? 'teal' : 'red'}
-      sx={{ display: 'flex', alignItems: 'center' }}
+      color={meets ? "teal" : "red"}
+      sx={{ display: "flex", alignItems: "center" }}
       mt={7}
       size="sm"
     >
       {meets ? <Check /> : <Crosshair />} <Box ml={10}>{label}</Box>
     </Text>
   );
-}
+};
 
-export {PasswordRequirement, getStrength, requirements};
+export { PasswordRequirement, getStrength, requirements };
